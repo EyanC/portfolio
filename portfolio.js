d47a9fd1d117cf50c22d0cd9fa5cfc8336301819
Eyan Cea <eyan.moses@gmail.com>
2022-11-01 20:15:34 -0500

    girst port update

---
+++ portfolio.js
@@ -0,0 +1,114 @@
+$(() => {
+const $navigationPanel = [
+    {
+        buttonClicked: 'Home', 
+    }, 
+    {
+        buttonClicked: 'Biography',
+    },
+    {
+        buttonClicked: 'Resumer'
+    }, 
+    {
+        buttonClicked: 'Projects',
+    },
+    {
+        buttonClicked: 'Links',
+    }
+]
+//created an array of button functions
+
+
+const $ulBiography = $('<ul>') 
+
+const $li = $('<li>').addClass('navigation')
+
+    // let $div = $('<div>').addClass(flex-container)
+    let $button = $('<button>').addClass(flex-container).text('Biograpghy')
+    //append
+}
+
+
+
+
+//needs loop to loop back to main page from other button tabs once 1 of 4 other buttons are pressed.
+const $home = () => {
+
+}
+
+const biography = () => {
+
+}
+
+//link direct link to resume when button is clicked
+const resume = () => {
+
+}
+
+const  projects = () => {
+
+} 
+
+const  links = () => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+})
+
+// ///////////              Creating Buttons
+    
+
+// <div id="button-navigation">
+// <button id="home">Home</button>
+// <button id="biography">Biography</button>
+// {/* <button id="resume">Resume</button> */}
+// <button id="projects">Projects</button>
+// <button id="links">Links</button> 
+
+// <button id="resume">Resume
+
+
+// </button>
+
+
+// </div>
+
+// //attaching a link to the Resume button
+// //
+
+
+
+
+
+// // $('mainPage').on('click', Rreseume)
+
+
+
+
+
+// //Errors
+// // let box = document.createElement('div')
+// // //First page, Into, Four Buttons, Picture
+
+// // const $navigationPanel = $('<div>').addClass('square')
+// // $('body').append($square);
+
+
+
+// // const mainPageButtons = (mainContent) => {
+// //     let $div = $('<div>').addClass('flex-button-box')
+// //     $('#flex-button-box').css('display', 'flex');
+
+
+// // }
+// // $div.append()
